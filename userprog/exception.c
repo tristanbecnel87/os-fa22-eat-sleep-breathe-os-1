531c30b9b7f20790a8eb8393225315701918e15d
suha0825 <suhaakhund@gmail.com>
2022-11-08 14:27:32 -0600

    vm 4 fails

--- userprog/exception.c
+++ userprog/exception.c
@@ -206,6 +206,8 @@ page_fault (struct intr_frame *f)
       f->eip = (void *) f->eax;
       f->eax = 0xffffffff;
 
+      exit(-1);
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
